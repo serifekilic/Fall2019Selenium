14e61b4e350c4ad6d59d29cfa65fb7cd0f64e5f6
serifekilic <purdemirderya@yahoo.com>
2020-03-08 12:09:16 -0400

    Select by text checking everymonth is working

--- src/test/java/com/automation/tests/day6/SelectByText.java
+++ src/test/java/com/automation/tests/day6/SelectByText.java
@@ -1,17 +1,16 @@
 package com.automation.tests.day6;
-
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class SelectByText {
+
     public static void main(String[] args) {
-
-
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/dropdown");
         BrowserUtils.wait(3);
@@ -21,11 +20,32 @@ public class SelectByText {
         Select selectSimpleDropdown = new Select(simpleDropdown);
         //select by visible text
         selectSimpleDropdown.selectByVisibleText("Option 2");
+        BrowserUtils.wait(3);
+        //and select option 1
+        selectSimpleDropdown.selectByVisibleText("Option 1");
+        //select you DOB
+        Select selectYear = new Select(driver.findElement(By.id("year")));
+        Select selectMonth = new Select(driver.findElement(By.id("month")));
+        Select selectDay = new Select(driver.findElement(By.id("day")));
 
+        selectDay.selectByVisibleText("1");
+        selectMonth.selectByVisibleText("February");
+        selectYear.selectByVisibleText("2003");
+        selectMonth.selectByVisibleText("February");
+        selectDay.selectByVisibleText("25");
 
+        BrowserUtils.wait(5);
 
+        //select all months one by one
+        //.getOptions(); - returns all options from dropdown as List<WebElement>
+        List<WebElement> months = selectMonth.getOptions();
+        for(WebElement eachMonth: months){
+            //get the month name and select based on that
+            String monthName = eachMonth.getText();
+            selectMonth.selectByVisibleText(monthName);
+            BrowserUtils.wait(1);
 
-
+        }
         BrowserUtils.wait(3);
         driver.quit();
     }
